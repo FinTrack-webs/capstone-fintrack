2de88c7d449a3508e29ed1364e360eb598f547f4
Alief albayu <aliefalbayu2@gmail.com>
2026-05-16 08:22:15 +0700

    chore: menambahkan struktur folder dan template .gitignore

--- Backend/src/config/jwt.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const jwtConfig = {
-  secret: process.env.JWT_SECRET || 'default_secret_fintrack',
-  accessTokenExpiry: '15m',   // Access token berlaku 15 menit
-  refreshTokenExpiry: '7d',   // Refresh token berlaku 7 hari
-};
-
-module.exports = jwtConfig;
